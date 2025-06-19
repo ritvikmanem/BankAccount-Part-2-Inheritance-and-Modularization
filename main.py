580f18a4b3e5e931e3b23e4ecbb14607b07f5983
Ritvik Manem <manemritvik@gmail.com>
2025-06-19 10:37:17 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,39 @@
+class BankAccount():
+    bank_name = "BOFA"
+    def __init__(self, customer_name, account_balance, minimum_balance):
+        self.customer_name = customer_name
+        self.account_balance = account_balance
+        self.minimum_balance = minimum_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+            print(f"Deposited {amount}. New balance: {self.account_balance}")
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        if amount > 0 and (self.account_balance - amount) >= self.minimum_balance:
+            self.account_balance -= amount
+            print(f"Withdrew {amount}. New balance: {self.account_balance}")
+        else:
+            print("Withdrawal amount exceeds the minimum balance requirement or is not positive.")
+
+    def print_customer_information(self):
+        print(f"Customer Name: {self.customer_name}")
+        print(f"Account Balance: {self.account_balance}")
+        print(f"Minimum Balance: {self.minimum_balance}")
+        print(f"Bank Name: {self.bank_name}")
+    
+
+bill = BankAccount("Bill", 1000, 100)
+bill.print_customer_information()
+bill.deposit(500)
+bill.withdraw(200)
+bill.print_customer_information()
+
+bob = BankAccount("Bob", 2000, 200)
+bob.print_customer_information()
+bob.withdraw(500)
+bob.deposit(300)
+bob.print_customer_information()
